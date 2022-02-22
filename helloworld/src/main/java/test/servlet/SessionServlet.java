27ea08d60748aa6c061450be5aa264aaaf97410d
jyokosaw <jyokosaw@redhat.com>
2022-02-22 17:06:24 +0900

    collected.

--- helloworld/src/main/java/test/servlet/SessionServlet.java
+++ helloworld/src/main/java/test/servlet/SessionServlet.java
@@ -5,7 +5,10 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/SessionServlet")
 public class SessionServlet extends HttpServlet {
